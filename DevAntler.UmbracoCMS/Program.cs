ba0f0c2226433edc996c9bc3202eb9a5c2c5e9f0
devantler <nikolaiemildamm@icloud.com>
2022-07-05 21:38:04 +0200

    Moved stuff around

---
+++ DevAntler.UmbracoCMS/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace DevAntler.UmbracoCMS;
+
+public static class Program
+{
+    public static void Main(string[] args)
+        => CreateHostBuilder(args)
+            .Build()
+            .Run();
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureUmbracoDefaults()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStaticWebAssets();
+                webBuilder.UseStartup<Startup>();
+            });
+}
+
+
+
+
+
+
+
+
+
+
